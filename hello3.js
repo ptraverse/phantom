c8ddb0a5fd2a550c892a31e00514dd99c2e4566a
Philippe Traverse <philippe.traverse@gmail.com>
2015-07-15 02:51:21 -0700

    Some late night fun

--- hello3.js
+++ hello3.js
@@ -5,16 +5,21 @@ page.onConsoleMessage = function(msg) {
 	console.log('console> ' + msg);
 };
 
-page.open(url, function(status) {	
+
+
+page.open(url, function(status) {
+
+  	page.injectJs("./jquery.min.js");
 
 	page.evaluate(function() {
 
 		var getNextPageButton = function() {
+			console.log('getting page');
 			var nextPageButton = $('.pagination-wrapper ul li:not(.disabled):not(.active)').first();
-			if (nextPageButton.length==0) {
+			if (nextPageButton.length===0) {
 				return false;
 			} else {
-				console.log("clicking page #"+nextPageButton.text());
+				console.log("getting next page #"+nextPageButton.text());
 				return nextPageButton;
 			}
 		};
@@ -22,33 +27,46 @@ page.open(url, function(status) {
 		var getAthleteData = function() {
 			var athleteRow = $('tr.ui-widget-content');
 			var athleteData = [];
-			$(athleteRow).each(function(index, row) {				
-				var athleteName = $(row).find('a.athlete-trigger').text();							
-				var athleteTime = $(row).find('td').last().text();				
+			$(athleteRow).each(function(index, row) {
+				var athleteName = $(row).find('a.athlete-trigger').text();
+				var athleteTime = $(row).find('td').last().text();
 				if (athleteName > '') {
 					var tuple = {'name': athleteName, 'time': athleteTime};
 					athleteData.push(tuple);
 				}
-			});			
+			});
 			return athleteData;
 		};
 
+		var getRect = function(element) {
+		    return $(element)[0].getBoundingClientRect();
+		};
+
 		// --------------------------- //
-		var nextPageButton = getNextPageButton();
+		var nextPageButton = true;
 		var athleteData = [];
-		var max = 2;
-		while (nextPageButton!=false && max>0) {
-			athleteData.push(getAthleteData());						
-			$(nextPageButton).click();
+		var maxPages = 6;
+		var pageNum = 0;
+
+		while (nextPageButton!==false && pageNum<maxPages) {
 			nextPageButton = getNextPageButton();
-			console.log(athleteData.length);
-			max = max-1;
+			// console.log(JSON.stringify(nextPageButton));
+			// var pageAthleteData = getAthleteData();
+			// athleteData.push(pageAthleteData);
+			// $(nextPageButton).click();
+
+			// var rect = getRect(nextPageButton);
+			// console.log(JSON.stringify(rect));
+			// page.sendEvent('click', rect.left + rect.width / 2, rect.top + rect.height / 2);
+
+			$(nextPageButton).trigger("click");
+			window.setTimeout(1000);
+
+			pageNum++;
 		}
 		console.log(JSON.stringify(athleteData));
 		// --------------------------- //
 	});
 
-
 	phantom.exit();
-
 });
---
+++ readme.md
@@ -0,0 +1,14 @@
+Playing with PhantomJS
+
+# Build
+<code>
+git clone https://github.com/ptraverse/phantom.git
+</code>
+
+<code>
+npm install
+</code>
+
+<code>
+./node_modules/phatomjs/bin/phantomjs <filename>.js
+</code>
